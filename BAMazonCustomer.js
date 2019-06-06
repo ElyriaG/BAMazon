5f2016b644bfc669063a6cf81aa8dbc43d5c5c38
ElyriaG <ElyriaG@users.noreply.github.com>
2019-06-05 23:55:35 -0500

    Challenge One Done

--- BAMazonCustomer.js
+++ BAMazonCustomer.js
@@ -17,9 +17,44 @@ connection.connect(function (err) {
 });
 
 function afterConnection() {
-  connection.query("SELECT * FROM products", function(err, res) {
-    if (err) throw err;
-    console.table(res)
-    connection.end();
-  });
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        console.table(res)
+        productInquiry();
+    });
+}
+
+function productInquiry() {
+    connection.query("SELECT * FROM products", function (error, results) {
+        inquirer.prompt([{
+            message: "What is the item id of the product you'd wish to purchase?",
+            name: "itemID",
+            type: "list",
+            choices: results.map(item => item.item_id)
+        },
+        {
+            message: "How many units do you wish to purchase?",
+            name: "units",
+            type: "number"
+        }])
+            .then(answers => {
+                let itemToBuy = results.find(item => item.item_id == answers.itemID);
+                let stockDeduction = (itemToBuy.stock_quantity - answers.units);
+                if (itemToBuy.stock_quantity < answers.units) {
+                    console.log("We do not currently have enough in stock. Sorry.");
+                } else {
+                    connection.query("UPDATE products SET ? WHERE ?", [{
+                        stock_quantity: stockDeduction
+                    }, {
+                        item_id: itemToBuy.item_id
+                    }], (error, response) => {
+                        if(error) console.log(error);
+                        // console.log(response);
+                        console.log(`Yay! You bought ${answers.units} units!`);
+                        // console.table(results)
+                        console.log(`Your price: $${answers.units * itemToBuy.price}`);
+                    })
+                };
+            })
+    })
 }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # BAMazon
+
+A very simple MySQL program. 
+
+Table was created on MySQL, and with JS and node, you can choose and buy a product. The total price will display and the stock will update on MySQL. 
